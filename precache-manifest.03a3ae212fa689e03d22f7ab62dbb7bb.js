f18bd6c7795a6e28408750210b1af4c000ea179f
Sathittham Sangthong <s.sathittham@gmail.com>
2020-02-13 19:13:01 +0700

    Updates

---
+++ precache-manifest.03a3ae212fa689e03d22f7ab62dbb7bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80cfafeb6b479d3b9fb65a00f81eef0a",
+    "url": "/geekracingtattoo/index.html"
+  },
+  {
+    "revision": "d96376843a500fc0b215",
+    "url": "/geekracingtattoo/static/css/2.0cdb9ae6.chunk.css"
+  },
+  {
+    "revision": "5df8cad3cabbaff39838",
+    "url": "/geekracingtattoo/static/css/main.e6b9b06c.chunk.css"
+  },
+  {
+    "revision": "d96376843a500fc0b215",
+    "url": "/geekracingtattoo/static/js/2.75c1b652.chunk.js"
+  },
+  {
+    "revision": "5df8cad3cabbaff39838",
+    "url": "/geekracingtattoo/static/js/main.5f2c6461.chunk.js"
+  },
+  {
+    "revision": "e3971393a73b5dae9214",
+    "url": "/geekracingtattoo/static/js/runtime~main.7ab5d70d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/geekracingtattoo/static/media/logo.5d5d9eef.svg"
+  }
+]);
